ffb5e7147aa573d7ee6a26436596f370f39fa314
Garrett Brown <garrettpaulbrown@gmail.com>
2021-06-19 19:22:31 -0700

    Models added, can access splash page.'

---
+++ Bakery/Models/BakeryContext.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Bakery.Models
+{
+  public class BakeryContext : DbContext
+  {
+    public virtual DbSet<Treat> Treats { get; set; }
+    public DbSet<Flavor> Flavors { get; set; }
+    public DbSet<TreatFlavor> TreatFlavor { get; set; }
+
+    public BakeryContext(DbContextOptions options) : base(options) { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseLazyLoadingProxies();
+    }
+  }
+}
